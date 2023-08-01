6185059ea24b111cf776519cff3f412d17cd5d51
Davi Nakano <davinakanoca@gmail.com>
2023-08-01 16:08:05 -0700

    chore: a little styling on blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,9 @@
-import { Metadata, ResolvingMetadata } from "next";
+import { Metadata } from "next";
 import { fetchBlogPost, fetchBlogPosts } from "@/contentful/blogPosts";
 import { draftMode } from "next/headers";
 import { notFound } from "next/navigation";
 import RichText from "@/contentful/RichText";
+import styles from "./styles.module.css";
 
 interface BlogPostPageParams {
   slug: string;
@@ -46,22 +47,26 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
   }
 
   return (
-    <section>
-      {blogPost.image && (
-        // Using Contentful Images API with srcSet
-        // eslint-disable-next-line @next/next/no-img-element
-        <img
-          src={blogPost.image.src}
-          srcSet={`${blogPost.image.src}?w=300 1x, ${blogPost.image.src} 2x`}
-          width={300}
-          height={300}
-          alt={blogPost.image.alt}
-        />
-      )}
+    <section className={`container ${styles.section}`}>
+      <div className={styles.imageContainer}>
+        {blogPost.image && (
+          // Using Contentful Images API with srcSet
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={blogPost.image.src}
+            srcSet={`${blogPost.image.src}?w=550 1x, ${blogPost.image.src} 2x`}
+            width={550}
+            height={300}
+            alt={blogPost.image.alt}
+          />
+        )}
+      </div>
 
-      <h1>{blogPost.title}</h1>
+      <div className={styles.textContainer}>
+        <h1>{blogPost.title}</h1>
 
-      <RichText document={blogPost.body} />
+        <RichText document={blogPost.body} />
+      </div>
     </section>
   );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,31 @@
-import { draftMode } from 'next/headers'
-import { fetchBlogPosts } from '@/contentful/blogPosts'
-import Link from 'next/link'
+import { draftMode } from "next/headers";
+import { fetchBlogPosts } from "@/contentful/blogPosts";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./styles.module.css";
 
 export default async function Blog() {
-  const blogPosts = await fetchBlogPosts({ preview: draftMode().isEnabled })
+  const blogPosts = await fetchBlogPosts({ preview: draftMode().isEnabled });
 
   return (
-    <section>
+    <section className={`container ${styles.section}`}>
+      <h1>Blog</h1>
       <ul>
-        {blogPosts.map(({ slug, title }) => (
+        {blogPosts.map(({ slug, title, shortDescription, image }) => (
           <li key={slug}>
-            <Link href={`/blog/${slug}`}>{title}</Link>
+            <Link href={`/blog/${slug}`}>
+              <div className={styles.imageContainer}>
+                {image && (
+                  <Image
+                    src={`https:${image.src}`}
+                    alt={image.alt}
+                    fill
+                    style={{ objectFit: "cover" }}
+                  />
+                )}
+              </div>
+              <h2>{title}</h2>
+            </Link>
           </li>
         ))}
       </ul>
